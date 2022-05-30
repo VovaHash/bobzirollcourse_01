9da64eddac3c98bc35c5efe4ef82b25284e53dbf
VovaHash <vovahash01@gmail.com>
2022-05-31 01:19:43 +0300

    font and start of styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,14 @@
+import logo from '../images/react-icon-small.png'
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+        <header>
+            <nav>
+            <img src={logo}/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+            </nav>
+           
+        </header>
+    )
 }
