6217f5581d85cef4bcaaa91197c51c7818a110a6
VovaHash <vovahash01@gmail.com>
2022-05-31 23:21:53 +0300

    final for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import logo from '../images/react-icon-small.png'
 
 export default function Navbar() {
     return (
-        <header>
+       
             <nav>
-            <img src={logo}/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={logo} className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
             </nav>
            
-        </header>
+        
     )
 }
